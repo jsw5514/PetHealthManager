64d0ac03c304fd4d76e4d0f998955cff0da65bfe
jsw5514 <jsw5524@gmail.com>
2025-04-08 01:01:51 +0900

    TinyGPS++ 라이브러리 추가, 추가한 라이브러리를 이용하여 GPS 모듈의 raw data를 파싱하여 출력하도록 수정

--- Arduino/ex_gps/ex_gps.ino
+++ Arduino/ex_gps/ex_gps.ino
@@ -1,24 +1,43 @@
+#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-//for VK2828U7G5LF
 
-// 소프트웨어 시리얼 객체 생성 (TX: 10, RX: 11)
-SoftwareSerial mySerial(4, 5);
+//TinyGPSPlus 객체 생성
+TinyGPSPlus gps;
+
+// 소프트웨어 시리얼 포트 생성 (RX: 4, TX: 5)
+SoftwareSerial GPSSerial(4, 5);
+
+// GPS 데이터 처리 상태 추적용 변수
+long validDataCount = 0; //유효한 데이터
+long totalReceivedCount = 0; //수신한 총 데이터
 
 void setup() {
-    // 하드웨어 시리얼 포트를 초기화하고 보율을 설정합니다
     Serial.begin(9600);
-    // 소프트웨어 시리얼 포트를 초기화하고 보율을 설정합니다
-    mySerial.begin(9600);
+    GPSSerial.begin(9600);
+    Serial.println("GPS 데이터 모니터링 시작");
 }
 
 void loop() {
-    // 소프트웨어 시리얼 포트에서 데이터를 읽습니다
-    if (mySerial.available() > 0) {
-        // 읽은 데이터를 하드웨어 시리얼 포트로 출력합니다
-        while (mySerial.available() > 0) {
-            char data = mySerial.read();
-            Serial.print(data);
-        }
-        //Serial.println();
+    while (mySerial.available() > 0) {
+        char c = GPSSerial.read();
+        gps.encode(c); //TinyGPSPlus 객체에 GPS 모듈 출력 전달
+        totalReceivedCount++; //수신받은 총 데이터 수 카운트
+    }
+
+    // 유효한 위치 데이터가 업데이트되었는지 확인
+    if (gps.location.isUpdated()) {
+        validDataCount++;//유효한 데이터 수 카운트
+        Serial.print("Latitude: ");
+        Serial.println(gps.location.lat(), 6); //위도 소수점 6자리로 출력
+        Serial.print("Longitude: ");
+        Serial.println(gps.location.lng(), 6); //경도 소수점 6자리로 출력
+    }
+
+    // 수신 상태 출력
+    if (totalReceivedCount % 100 == 0) { // 100개의 문자를 처리할 때마다 상태 확인
+        Serial.print("Total Characters Received: ");
+        Serial.println(totalReceivedCount);
+        Serial.print("valid Data Count: ");
+        Serial.println(validDataCount);
     }
 }
