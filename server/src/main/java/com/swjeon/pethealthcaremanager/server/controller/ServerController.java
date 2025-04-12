179ce88e0c1b0889535de94a23131b2d3a7af7ae
jsw5514 <jsw55514@naver.com>
2025-04-12 16:24:26 +0900

    로그인 url 매핑 추가

--- server/src/main/java/com/swjeon/pethealthcaremanager/server/controller/ServerController.java
+++ server/src/main/java/com/swjeon/pethealthcaremanager/server/controller/ServerController.java
@@ -1,8 +1,6 @@
 package com.swjeon.pethealthcaremanager.server.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -11,6 +9,34 @@ public class ServerController {
 
     @GetMapping("/")
     public String index() {
-        return "서버 접속 성공. url을 수정하여 구체적인 요청을 명시할 것";
+        return "서버 접속 성공. url을 수정하여 구체적인 요청을 명시";
+    }
+
+    //id 중복확인
+    @RequestMapping(value = "/checkDuplicateId", method = {RequestMethod.GET, RequestMethod.POST})
+    public String checkDuplicateId(@RequestParam("id") String id) {
+        return "not yet implemented";//TODO not yet implemented
+    }
+
+    //회원가입
+    @PostMapping("/signIn")
+    public String signIn(@RequestParam("id") String id, @RequestParam("password") String password) {
+        return "not yet implemented";//TODO not yet implemented
+    }
+
+    //로그인
+    @PostMapping("/login")
+    public String login(@RequestParam("id") String id, @RequestParam("password") String password) {
+        return "not yet implemented";//TODO not yet implemented
+    }
+
+    //디버그용
+    @GetMapping("/signIn")
+    public String noSignIn(@RequestParam("id") String id, @RequestParam("password") String password) {
+        return "회원가입은 Post 매서드로 처리 id: " + id + " password: " + password;
+    }
+    @GetMapping("/login")
+    public String noLogin(@RequestParam("id") String id, @RequestParam("password") String password) {
+        return "로그인은 Post 매서드로 처리 id: " + id + " password: " + password;
     }
 }
