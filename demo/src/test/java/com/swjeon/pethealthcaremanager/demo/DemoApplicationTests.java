c5b9ec6199995be80b5a5b06dd6774a098eb5787
jsw5514 <jsw55514@naver.com>
2025-04-12 10:36:55 +0900

    demo project skeleton

---
+++ demo/src/test/java/com/swjeon/pethealthcaremanager/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swjeon.pethealthcaremanager.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
