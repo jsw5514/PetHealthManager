841afe0ab2239daebaaf0a6c44149051ec5c57cb
jsw5514 <jsw55514@naver.com>
2025-04-13 22:13:08 +0900

    채팅 기능을 위한 함수 기본틀 작성

--- server/src/main/java/com/swjeon/pethealthcaremanager/server/controller/ChatController.java
+++ server/src/main/java/com/swjeon/pethealthcaremanager/server/controller/ChatController.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 @RestController
 public class ChatController {
-    /**
+    /** 채팅 업로드 함수
      * @param roomId 채팅방 id
      * @param writerId 채팅 작성자 id
      * @param writeTime 채팅 작성 시간
@@ -14,9 +14,9 @@ public class ChatController {
      * @param content 채팅내용(바이너리 데이터는 Base64 인코딩 후 전송)
      * @return 채팅 업로드 성공여부(boolean)
      */
-    @PostMapping
+    @PostMapping("/uploadChat")
     public String uploadChat(
-            @RequestParam("roomId")int roomId,
+            @RequestParam("roomId") int roomId,
             @RequestParam("writerId") String writerId,
             @RequestParam("writeTime") LocalDateTime writeTime,
             @RequestParam("contentType") String contentType,
@@ -25,4 +25,37 @@ public class ChatController {
         return "not yet implemented"; //TODO not yet implemented
     }
 
+    /** 채팅 내용 갱신 함수
+     * @param roomId 채팅방 id
+     * @param latestTimestamp 마지막으로 데이터를 갱신한 시점
+     * @return 갱신된 채팅 내용(json 객체로 반환)
+     *          roomId: 채팅방 Id
+     *          contentList: 채팅 내용 배열(json 배열)
+     *              writerNickname: 작성자 닉네임
+     *              contentType: 채팅 내용 데이터 타입
+     *              content: 채팅내용(바이너리 데이터는 Base64 인코딩 후 전송)
+     */
+    @PostMapping("/downloadChat")
+    public String downloadChat(@RequestParam("roomId") int roomId, @RequestParam("latestTimestamp") LocalDateTime latestTimestamp) {
+        return "not yet implemented"; //TODO not yet implemented
+    }
+
+    /**
+     * @param creatorId 채팅방 생성자 id
+     * @return 생성된 채팅방 id(int,생성 실패시 0)
+     */
+    @PostMapping("/createChatRoom")
+    public String createChatRoom(@RequestParam("creatorId") String creatorId){
+        return "not yet implemented"; //TODO not yet implemented
+    }
+
+    /**
+     * @param roomId 초대할 채팅방 id
+     * @param memberId 초대할 사람의 id
+     * @return 성공여부(boolean)
+     */
+    @PostMapping("/inviteChatMember")
+    public String inviteChatMember(@RequestParam("roomId") int roomId, @RequestParam("memberId") String memberId) {
+        return "not yet implemented"; //TODO not yet implemented
+    }
 }
