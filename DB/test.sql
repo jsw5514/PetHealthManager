8f16a6477d70fc0c5884888261a765295ee92758
jsw5514 <jsw5524@gmail.com>
2025-04-15 12:01:35 +0900

    변경된 db 구조에 맞게 테스트 쿼리 수정

--- DB/test.sql
+++ DB/test.sql
@@ -1,14 +1,14 @@
 -- 테스트 데이터 삽입용
-INSERT INTO USERS VALUES("testid","testpw","testnickname"),("user1","user1","user1"),("user2","user2","user2"),("user3","user3","user3"),("user4","user4","user4");
-INSERT INTO CHAT_ROOM VALUES();
-INSERT INTO CHAT_ROOM VALUES();
+INSERT INTO USERS VALUES("testid","testpw","testnickname"),("user1","user1","user1"),("user2","user2","user2"),("user3","user3","user3"),("user4","user4","user4"),("user5","user5","user5");
+INSERT INTO CHAT_ROOM VALUES(); INSERT INTO CHAT_ROOM VALUES(); INSERT INTO CHAT_ROOM VALUES();
 INSERT INTO CHAT_MEMBER VALUES(1,"testid");
-INSERT INTO CHAT (ROOM_ID, WRITER_ID, CONTENT) 
-VALUES 
-(1, 'user1', '{"text": "Hello, world!"}'),
-(1, 'user2', '{"text": "How are you?"}'),
-(2, 'user3', '{"text": "Welcome to room 2"}'),
-(2, 'user4', '{"text": "Hi there!"}');
+INSERT INTO CHAT (ROOM_ID, WRITER_ID, CONTENT_TYPE, CONTENT_PATH)
+VALUES
+(1, 'user1', 'text', '/texts/hello_world.txt'),
+(1, 'user2', 'image', '/images/example1.png'),
+(2, 'user3', 'video', '/videos/example2.mp4'),
+(2, 'user4', 'text', '/texts/test_message.txt'),
+(3, 'user5', 'audio', '/audios/example3.mp3');
 
 -- 모든 데이터 삭제
 DROP TABLE CHAT_MEMBER;
