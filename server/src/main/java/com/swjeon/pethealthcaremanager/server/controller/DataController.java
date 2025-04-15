bd5e42beea2636fc676a20a21bbc892bfdf694f1
jsw5514 <jsw5524@gmail.com>
2025-04-15 13:37:16 +0900

    추가적인 데이터를 업로드하고 다운로드할 수 있도록 하는 컨트롤러 추가

---
+++ server/src/main/java/com/swjeon/pethealthcaremanager/server/controller/DataController.java
@@ -0,0 +1,8 @@
+package com.swjeon.pethealthcaremanager.server.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataController {
+}
