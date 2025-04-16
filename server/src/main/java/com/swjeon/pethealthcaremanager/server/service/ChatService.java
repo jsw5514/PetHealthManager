dbea3ece7e008e67948be7be28b95201ed2f3cc0
jsw5514 <jsw55514@naver.com>
2025-04-17 01:36:48 +0900

    파일 입출력은 파일서비스 클래스가 담당하도록 분리

--- server/src/main/java/com/swjeon/pethealthcaremanager/server/service/ChatService.java
+++ server/src/main/java/com/swjeon/pethealthcaremanager/server/service/ChatService.java
@@ -4,43 +4,31 @@ import com.swjeon.pethealthcaremanager.server.Entity.ChatEntity;
 import com.swjeon.pethealthcaremanager.server.Repository.ChatRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.time.LocalDateTime;
 
 @Service
 public class ChatService {
     private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
-    private final String CHAT_STORAGE=System.getProperty("user.dir")+"/storage/chat";
     private final ChatRepository chatRepository;
 
+    @Autowired
     public ChatService(ChatRepository chatRepository) {
         this.chatRepository = chatRepository;
     }
 
     public boolean uploadChat(int roomId, String writerId, LocalDateTime writeTime, String contentType, String content) 
     {
-        String timeString = writeTime.toString().replace(":","-");
-        final String chatPath = CHAT_STORAGE + "/" + writerId + "_" + roomId + "_" + timeString + ".txt";
-        log.info("content: " + content + " save_path: " + chatPath);
-        File file = new File(chatPath);
-
-        
-        //파일로 저장
-        try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            writer.write(content);
-            writer.flush();
-        }
-        catch (Exception e){
-            e.printStackTrace();
+        //채팅 내용 파일로 저장
+        final String timeString = writeTime.toString().replace(":","-");
+        final String fileName = writerId + "_" + roomId + "_" + timeString + ".txt";
+        final String chatPath = FileService.saveChat(content, fileName);
+        if (chatPath == null)
             return false;
-        }
-        
-        //db에 저장
+
+        //파일 경로 및 나머지 데이터 db에 저장
         ChatEntity chatEntity = new ChatEntity();
         chatEntity.setRoomId(roomId);
         chatEntity.setWriterId(writerId);
@@ -48,6 +36,7 @@ public class ChatService {
         chatEntity.setContentType(contentType);
         chatEntity.setContentPath(chatPath);
         chatRepository.save(chatEntity);
-        return true;//TODO not yet implemented
+        return true;
     }
+
 }
---
+++ server/src/main/java/com/swjeon/pethealthcaremanager/server/service/FileService.java
@@ -0,0 +1,37 @@
+package com.swjeon.pethealthcaremanager.server.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class FileService {
+    private static final Logger log = LoggerFactory.getLogger(FileService.class.getSimpleName());
+    private static final String CHAT_STORAGE=System.getProperty("user.dir")+"/storage/chat";
+
+
+    static boolean save(String content, String savePath) {
+        File file = new File(savePath);
+
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(content);
+            writer.flush();
+
+            log.debug("file saved. content: " + content + " save_path: " + savePath);
+        }
+        catch (Exception e){
+            log.error(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static String saveChat(String content, String fileName) {
+        String chatPath = CHAT_STORAGE + "/" + fileName;
+        boolean isSaved = save(content, chatPath);
+        return isSaved ? chatPath : null;
+    }
+}
