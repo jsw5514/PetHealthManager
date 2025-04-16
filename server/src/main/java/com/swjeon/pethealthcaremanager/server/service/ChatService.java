a231bd34db25d1abaabfab27fdfa4cfe40711e41
jsw5514 <jsw55514@naver.com>
2025-04-17 01:05:44 +0900

    채팅 내용 파일 저장 구현

--- server/src/main/java/com/swjeon/pethealthcaremanager/server/service/ChatService.java
+++ server/src/main/java/com/swjeon/pethealthcaremanager/server/service/ChatService.java
@@ -2,13 +2,19 @@ package com.swjeon.pethealthcaremanager.server.service;
 
 import com.swjeon.pethealthcaremanager.server.Entity.ChatEntity;
 import com.swjeon.pethealthcaremanager.server.Repository.ChatRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.time.LocalDateTime;
 
 @Service
 public class ChatService {
-    private final String LOCAL_STORAGE="/";// storage/chat";
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
+    private final String CHAT_STORAGE=System.getProperty("user.dir")+"/storage/chat";
     private final ChatRepository chatRepository;
 
     public ChatService(ChatRepository chatRepository) {
@@ -17,9 +23,22 @@ public class ChatService {
 
     public boolean uploadChat(int roomId, String writerId, LocalDateTime writeTime, String contentType, String content) 
     {
-        final String chatPath = LOCAL_STORAGE + writerId + "_" + roomId + "_" + writeTime;
+        String timeString = writeTime.toString().replace(":","-");
+        final String chatPath = CHAT_STORAGE + "/" + writerId + "_" + roomId + "_" + timeString + ".txt";
+        log.info("content: " + content + " save_path: " + chatPath);
+        File file = new File(chatPath);
+
         
-        //TODO content 내용을 파일로 저장 구현
+        //파일로 저장
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(content);
+            writer.flush();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
         
         //db에 저장
         ChatEntity chatEntity = new ChatEntity();
@@ -29,6 +48,6 @@ public class ChatService {
         chatEntity.setContentType(contentType);
         chatEntity.setContentPath(chatPath);
         chatRepository.save(chatEntity);
-        return false;//TODO not yet implemented
+        return true;//TODO not yet implemented
     }
 }
