efcac9f21327baf6ea5efa432fa893794e511a24
jsw5514 <jsw55514@naver.com>
2025-04-27 13:15:33 +0900

    버그 수정 및 보안 검사 추가 -파일을 다운로드할 경우 내부 저장소 경로가 일부 중복되어 정상적으로 파일을 불러올 수 없는 버그 수정 -파일을 다운로드할 경우 정상적인 내부 저장소 경로를 요구하는지 확인하는 보안 검사 추가

--- server/src/main/java/com/swjeon/pethealthcaremanager/server/service/FileService.java
+++ server/src/main/java/com/swjeon/pethealthcaremanager/server/service/FileService.java
@@ -70,10 +70,14 @@ public class FileService {
     }
 
     //채팅 불러오기
-    public static String loadChat(String chatFileName) {
-        String chatPath = CHAT_STORAGE + "/" + chatFileName;
-        String loadedContent = load(chatPath);
-        log.debug(chatPath + " loaded content: " + loadedContent);
+    public static String loadChat(String chatFilePath) {
+        //요구된 파일의 경로가 정상적인 데이터 파일 경로인지 확인
+        if (!chatFilePath.startsWith(DATA_STORAGE)) {
+            log.error("invalid chat file path: " + chatFilePath);
+            return null;
+        }
+        String loadedContent = load(chatFilePath);
+        log.info("chat file loaded in path: " + chatFilePath + " content: " + loadedContent);
         return loadedContent;
     }
 
@@ -85,10 +89,14 @@ public class FileService {
     }
     
     //데이터 불러오기
-    public static String loadData(String loadFileName) {
-        String dataPath = DATA_STORAGE + "/" + loadFileName;
-        String loadedContent = load(dataPath);
-        log.info(dataPath + " loaded content: " + loadedContent);
+    public static String loadData(String dataFilePath) {
+        //요구된 파일의 경로가 정상적인 데이터 파일 경로인지 확인
+        if (!dataFilePath.startsWith(DATA_STORAGE)) {
+            log.error("invalid data file path: " + dataFilePath);
+            return null;
+        }
+        String loadedContent = load(dataFilePath);
+        log.info("data file loaded in path: " + dataFilePath + " content: " + loadedContent);
         return loadedContent;
     }
 }
