8e45bdf4e4c99bbbff5912a3f2fa8c4a539144fc
jsw5514 <jsw5524@gmail.com>
2025-05-27 14:59:40 +0900

    (admin) 관리자 페이지를 위한 관리자 서버 skeleton

---
+++ admin/src/test/java/com/swjeon/pethealthcaremanager/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swjeon.pethealthcaremanager.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
