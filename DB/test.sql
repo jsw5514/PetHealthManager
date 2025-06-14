9541215922b47d25714f13cef763a2b0540d635a
jsw5514 <jsw55514@naver.com>
2025-06-14 21:33:37 +0900

    DB 수정: 채팅 데이터 바디 칼럼명 수정, 채팅 작성시간 타임스탬프에 기본값 부여 삭제

--- DB/test.sql
+++ DB/test.sql
@@ -2,14 +2,14 @@
 INSERT INTO USERS VALUES("testid","testpw","testnickname"),("user1","user1","user1"),("user2","user2","user2"),("user3","user3","user3"),("user4","user4","user4"),("user5","user5","user5");
 INSERT INTO CHAT_ROOM VALUES(); INSERT INTO CHAT_ROOM VALUES(); INSERT INTO CHAT_ROOM VALUES();
 INSERT INTO CHAT_MEMBER VALUES(1,"testid");
-INSERT INTO CHAT (ROOM_ID, WRITER_ID, CONTENT_TYPE, CONTENT_PATH)
+INSERT INTO CHAT (ROOM_ID, WRITER_ID, CONTENT_TYPE, CONTENT)
 VALUES
-(1, 'user1', 'text', '/texts/hello_world.txt'),
+(1, 'user1', 'text', 'this is test message'),
 (1, 'user2', 'image', '/images/example1.png'),
 (2, 'user3', 'video', '/videos/example2.mp4'),
-(2, 'user4', 'text', '/texts/test_message.txt'),
+(2, 'user4', 'text', 'this is test message 2'),
 (3, 'user5', 'audio', '/audios/example3.mp3');
-INSERT INTO DATA (UPLOADER_ID, DATA_ID, META_DATA, DATA_PATH) VALUES ("testid", "testDataId", "just test data", "/path/for/test/data");
+INSERT INTO DATA (UPLOADER_ID, DATA_ID, META_DATA, CONTENT) VALUES ("testid", "testDataId", "just test data", '{ "content":"this is json content"}');
 
 -- 데이터 출력용
 SELECT * FROM USERS;
