8694ba9774d5dd5a5a20cc466e15042eb9b0573c
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2025-08-06 19:20:44 +0900

    (반려동물 기능 수정)클래스별 책임 정리

--- server/src/main/java/com/swjeon/pethealthcaremanager/server/Controller/PetController.java
+++ server/src/main/java/com/swjeon/pethealthcaremanager/server/Controller/PetController.java
@@ -1,14 +1,18 @@
 package com.swjeon.pethealthcaremanager.server.Controller;
 
 import com.swjeon.pethealthcaremanager.server.Service.PetService;
+import com.swjeon.pethealthcaremanager.server.dto.PetDTO;
+import com.swjeon.pethealthcaremanager.server.util.ExceptionUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/pet")
 public class PetController {
@@ -18,9 +22,42 @@ public class PetController {
         this.petService = petService;
     }
 
+    @PostMapping("/profile")
+    public ResponseEntity<Void> uploadPetProfile(@RequestBody PetDTO petDTO) {
+        log.info("Upload attempt with pet: " + petDTO);
+        try {
+            petService.uploadPetProfile(petDTO);
+        }
+        catch (Exception e) {
+            throw switch (ExceptionUtil.getErrorCode(e)) {
+                case ExceptionUtil.FOREIGN_KEY_EXCEPTION -> {
+                    log.error("반려동물 프로필 저장중 외래키 예외 발생. " +
+                            "반려동물 주인의 user id가 user 테이블에 존재하지 않음");
+                    yield new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 유저에 대한 반려동물 추가입니다.");
+                }
+                case ExceptionUtil.UNIQUE_KEY_EXCEPTION -> {
+                    log.error("반려동물 프로필 저장중 유니크키 예외 발생. " +
+                            "pet id에 해당하는 데이터가 이미 존재함.");
+                    yield new ResponseStatusException(HttpStatus.BAD_REQUEST, "pet id가 중복되었습니다. 다른 pet id를 사용하세요.");
+                }
+                default -> {
+                    log.error("알 수 없는 예외입니다. 예외: " + e);
+                    yield new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 오류가 발생했습니다. 서버 로그를 확인해주세요.");
+                }
+            };
+        }
+        return ResponseEntity.ok().build();
+    }
+
     @DeleteMapping("/profile")
     public ResponseEntity<Void> removePetProfile(@RequestBody Map<String, String> request) {
         String petId = request.get("petId");
-        return petService.removePetProfile(petId);
+        try {
+            petService.removePetProfile(petId);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "삭제할 프로필을 찾을 수 없습니다.");
+        }
+        return ResponseEntity.ok().build();
     }
 }
--- server/src/main/java/com/swjeon/pethealthcaremanager/server/Service/PetService.java
+++ server/src/main/java/com/swjeon/pethealthcaremanager/server/Service/PetService.java
@@ -1,17 +1,11 @@
 package com.swjeon.pethealthcaremanager.server.Service;
 
+import com.swjeon.pethealthcaremanager.server.Entity.PetEntity;
 import com.swjeon.pethealthcaremanager.server.Repository.PetRepository;
-import io.micrometer.observation.Observation;
+import com.swjeon.pethealthcaremanager.server.dto.PetDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.sql.SQLIntegrityConstraintViolationException;
 
 @Service
 public class PetService {
@@ -22,32 +16,13 @@ public class PetService {
         this.petRepository = petRepository;
     }
 
-    public ResponseEntity<Void> removePetProfile(String petId) {
-        try {
-            petRepository.deleteById(petId);
-        }
-        catch (Throwable e){
-            if (e instanceof EmptyResultDataAccessException) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "삭제할 프로필을 찾을 수 없습니다.");
-            }
-            else if (e instanceof DataIntegrityViolationException) {
-                Throwable root = e.getCause();
-                while (root.getCause() != null) {
-                    root = root.getCause();
-                }
-                if (root instanceof SQLIntegrityConstraintViolationException ex) {
-                    switch (ex.getErrorCode()) {
-                        case 1451:
-                            //외래키 위반(일반적으로는 발생하지 않음)
-                            log.error("외래키 위반 발생. 삭제 대상 데이터를 다른 테이블이 참조하고 있습니다.");
-                            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류 발생. 로그를 확인해주세요.");
-                        default:
-                            log.error("알 수 없는 오류입니다.");
-                            throw new RuntimeException(ex);
-                    }
-                }
-            }
-        }
-        return ResponseEntity.ok().build();
+
+    public void uploadPetProfile(PetDTO petDTO) {
+        PetEntity entity = petDTO.toEntity();
+        petRepository.save(entity);
+    }
+
+    public void removePetProfile(String petId) {
+        petRepository.deleteById(petId);
     }
 }
